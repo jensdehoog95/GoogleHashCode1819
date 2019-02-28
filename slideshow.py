26e7ffe1f02d202860529a11be6e6a137877861d
Jens de Hoog <jensdehoog@icloud.com>
2019-02-28 19:40:08 +0100

    Added output parser to slideshow

--- slideshow.py
+++ slideshow.py
@@ -1,5 +1,8 @@
 from slide import Slide
 
+output_file = "output.txt"
+
+
 class Slideshow():
     def __init__(self):
         self.slides = []
@@ -8,4 +11,27 @@ class Slideshow():
         self.slides.append(slide)
 
     def parse_output(self):
-        pass
+        with open(output_file, "w") as f:
+            num_slides = self._get_num_slides()
+            self._write_line(f, str(num_slides) + '\n')
+
+            for x in range(len(self.slides)):
+                slide = self.slides[x]
+
+                tmp_photo = slide.photo1
+                if tmp_photo is not None:
+                    self._write_line(f, str(tmp_photo.idx))
+                else:
+                    raise ValueError('Photo 1 in slide does not exist')
+
+                tmp_photo = slide.photo2
+                if tmp_photo is not None:
+                    self._write_line(f, ' ' + tmp_photo.idx)
+
+                self._write_line('\n')
+
+    def _get_num_slides(self):
+        return len(self.slides)
+
+    def _write_line(self, file, line):
+        file.write(line)
