1b2e654fa811eb8a378c346dc5fd4175d7b212a8
Reinout <reinout.eyckerman@uantwerpen.be>
2019-02-28 19:27:36 +0100

    Added tags to slides

--- slide.py
+++ slide.py
@@ -4,10 +4,7 @@ class Slide():
     def __init__(self, photo1: Photo=None, photo2: Photo=None):
         self.photo1 = photo1
         self.photo2 = photo2
-
-    def get_tags(self):
-        tags=[]
-        tags.extend(self.photo1.tags)
-        tags.extend(self.photo2.tags)
-        return tags
+        self.tags=[]
+        self.tags.extend(self.photo1.tags)
+        self.tags.extend(self.photo2.tags)
 
