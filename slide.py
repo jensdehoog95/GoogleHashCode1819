043c9f63b93440d0bf9fb478b31828fd8933354c
Jens de Hoog <jensdehoog@icloud.com>
2019-02-28 19:48:26 +0100

    Added catch for non-existing Photo2

--- slide.py
+++ slide.py
@@ -1,10 +1,15 @@
 from photo import Photo
 
 class Slide():
-    def __init__(self, photo1: Photo=None, photo2: Photo=None):
+    def __init__(self, photo1=None, photo2=None):
         self.photo1 = photo1
         self.photo2 = photo2
+
         self.tags=[]
-        self.tags.extend(self.photo1.tags)
-        self.tags.extend(self.photo2.tags)
+
+        if photo1 is not None:
+            self.tags.extend(self.photo1.tags)
+
+        if photo2 is not None:
+            self.tags.extend(self.photo2.tags)
 
