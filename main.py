1dc8946f89cfefd0c0189dc2d2a98ed302f501c5
Jens de Hoog <jensdehoog@icloud.com>
2019-02-28 19:48:42 +0100

    Added slideshow and parser to main

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from input_parser import read_file
 from collection import Collection
+from slideshow import Slideshow
+from photo import Photo
+from slide import Slide
 
 filename = "a_example.txt"
 
 
 def main():
     collection = Collection()
+    slideshow = Slideshow()
     read_file(filename, collection)
+
+    slideshow.parse_output()
+
     print("Done")
 
 
